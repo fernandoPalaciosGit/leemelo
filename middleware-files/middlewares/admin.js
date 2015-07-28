ef0b666fbbe741998286cce74c847acd6d919ac8
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2015-07-28 22:51:53 +0200

    swig template as main engine render

--- middleware-files/middlewares/admin.js
+++ middleware-files/middlewares/admin.js
@@ -1,4 +1,5 @@
 // list of middlewares that express use
 module.exports = {
-  static: require('./static.middleware') 
+  static: require('./static.middleware'),
+  favicon: require('./favicon.middleware')
 };
--- middleware-files/server/expressServer.js
+++ middleware-files/server/expressServer.js
@@ -1,5 +1,8 @@
+'use strict';
+
 var express = require('express'),
     middlewares = require('../middlewares/admin'),
+    swig = require('swig'),
     ExpressServer = function (conf) {
         this.conf = conf || {};
         this.expressServer = express();
@@ -9,9 +12,16 @@ var express = require('express'),
             this.expressServer.use(middlewares[middleware]);
         }
         
+        // Swig Templates
+        this.expressServer.engine('html', swig.renderFile);
+        this.expressServer.set('view engine', 'html'); // template of view
+        this.expressServer.set('views', __dirname + './../website/views/templates'); // html templates paths
+        
+        // Routing
         this.expressServer.get('/article/save', function (req, res, next) {
-            res.send('loading....article/save');
+            res.render('article_save', {name: 'Pocoyo'});
         });
+        
         this.expressServer.get('/article/list', function (req, res, next) {
             res.send('loading....article/list');
         });
