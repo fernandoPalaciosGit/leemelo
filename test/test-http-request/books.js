6c972e19a991e4c38807d18ca1dac62d5449145e
fernandoPalaciosGit <fernandopalacioslandi@gmail.com>
2015-08-18 22:57:46 +0200

    mocha testing, GET book/idBook

--- test/test-http-request/books.js
+++ test/test-http-request/books.js
@@ -8,12 +8,12 @@
             
         describe('[POST]', function () {
             
-            it('Should create new book or update', function (done) {
+            it('Should create new book or update.', function (done) {
                 var data = {
                     book: {
                         isbn: '123456789',
                         title: 'Title book',
-                        description: 'description book'
+                        description: 'Description book'
                     }  
                 };
                 
@@ -22,7 +22,7 @@
                     .post('/book/save')
                     .set('Accept', 'application/json')
                     .send(data)
-                    .expect(200)
+                    .expect(201)
                     .expect('Content-Type', /application\/json/)
                     .end(function (err, res) {
                         var body = res.body,
@@ -31,8 +31,47 @@
                         expect(body).to.have.property('book');
                         expect(book).to.have.property('isbn', '123456789');
                         expect(book).to.have.property('title', 'Title book');
-                        expect(book).to.have.property('description', 'description book');
+                        expect(book).to.have.property('description', 'Description book');
+                        expect(book).to.have.property('id');
                         done();
+                    });
+            });
+        });
+
+        describe('[GET]', function () {
+            it('Should retrieve an existance book.', function (done) {
+                var data = {
+                    book: {
+                        isbn: '123456789',
+                        title: 'Title book',
+                        description: 'Description book'
+                    }  
+                };
+                
+                data = JSON.stringify(data);
+                testServer
+                    .post('/book/save')
+                    .set('Accept', 'application/json')
+                    .send(data)
+                    .expect(201)
+                    .end(function (err, res) {
+                        var idBook = res.body.book.id;
+                            
+                        testServer
+                            .get('/book/' + idBook)
+                            .expect(200)
+                            .expect('Content-Type', /application\/json/)
+                            .end(function (err, res) {
+                                var body = res.body,
+                                    book = body.book;
+                                            
+                                expect(body).to.have.property('book');
+                                expect(book).to.have.property('isbn', '123456789');
+                                expect(book).to.have.property('title', 'Title book');
+                                expect(book).to.have.property('description', 'Description book');
+                                expect(book).to.have.property('id', idBook);
+                                done();
+                            });
                     });
             });
         });
