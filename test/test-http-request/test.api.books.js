4b8e23ad88f07c4b3f8efeb43ce7dc70af028f83
fernandoPalaciosGit <fernandopalacioslandi@gmail.com>
2015-08-24 22:39:41 +0200

    testing delete restfull api books

--- test/test-http-request/test.api.books.js
+++ test/test-http-request/test.api.books.js
@@ -108,10 +108,50 @@
                         var headerParams = res.body,
                             bookParams = headerParams.book;
                         
-                        // Assertions ti Testing Book
+                        // Assertions Testing Book
                         expect(headerParams).to.have.property('book');
                         expect(bookParams).to.have.property('title').to.equals('Title book for Nerds');
                         expect(bookParams).to.have.property('id').to.equal(idBook);
+                    })
+                    .then(done)
+                    .catch(console.log.bind(console));
+            });
+        });
+
+        describe('[DELETE]', function () {
+            it('It should delete a book', function (done) {
+                 var idBook = null;
+                
+                // Create or update Book [POST]
+                testServer
+                    .post('/api/book/save/')
+                    .type('form')
+                    .send({book: _bookTest})
+                    .set('Accept', /application\/json/)
+                    .expect(201)
+                    .then(function (res) {
+                        idBook = res.body.book.id;
+                        
+                        // get The Book cerated before
+                        return testServer
+                            .get('/api/book/id/' + idBook)
+                            .expect(200)
+                            .expect('Content-Type', /application\/json/);
+                    })
+                    .then(function (res) {
+                        var headerParams = res.body,
+                            bookParams = headerParams.book;
+                        
+                        bookParams.title = 'Title book for Nerds';
+                        // remove a book [DELETE]
+                        testServer
+                            .delete('/api/book/id/' + idBook)
+                            .expect(200);    
+                    })
+                    .then(function () {
+                        testServer
+                            .get('/api/book/id/' + idBook)
+                            .expect(404);
                     })
                     .then(done)
                     .catch(console.log.bind(console));
