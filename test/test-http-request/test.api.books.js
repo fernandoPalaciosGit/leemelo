c9a0ba388122f7a640366c6708f4217868862ea4
fernandoPalaciosGit <fernandopalacioslandi@gmail.com>
2015-08-25 19:12:41 +0200

    set concurrent request for all test to create a new book

--- test/test-http-request/test.api.books.js
+++ test/test-http-request/test.api.books.js
@@ -6,148 +6,120 @@
     
     describe('Request /book/save', function () {
         var _bookTest = {
-                isbn: '123456789',
-                title: 'Title book',
-                description: 'Description book'
+                book: {
+                    isbn: '123456789',
+                    title: 'Title book',
+                    description: 'Description book'
+                }
             },
             _assertBookProperties = function (book) {
                 expect(book).to.have.property('isbn', '123456789');
                 expect(book).to.have.property('title', 'Title book');
                 expect(book).to.have.property('description', 'Description book');
                 expect(book).to.have.property('id');
+            },
+            _createNewBook = function () {
+                return testServer
+                    .post('/api/book/save/')
+                    .set('Accept', 'application/json')
+                    .send(_bookTest)
+                    .then(function (res) {
+                        var headerParams = res.body;
+                        
+                        expect(headerParams).to.have.property('book');
+                        this.newBook = headerParams.book;
+                    }.bind(this));
             };
+        
+        beforeEach(_createNewBook);
         
         describe('[POST]', function () {
             it('Should create new book or update.', function (done) {
-                testServer
-                    .post('/api/book/save/')
-                    .send({book: _bookTest})
-                    .set('Accept', 'application/json')
-                    .expect(201)
-                    .then(function (res) {
-                        var headerParams = res.body,
-                            bookParams = headerParams.book;
-                        
-                        expect(headerParams).to.have.property('book');    
-                        _assertBookProperties(bookParams);
-                    })
-                    .then(done)
-                    .catch(console.log.bind(console));
+                _assertBookProperties(this.newBook);
+                done();
             });
         });
 
         describe('[GET]', function () {
             it('Should retrieve an existance book.', function (done) {
-                var idBook = null;
+                var idBook = this.newBook.id;
                 
-                // Create or update Book
-                testServer
-                    .post('/api/book/save/')
-                    .send({book: _bookTest})
-                    .set('Accept', /application\/json/)
-                    .expect(201)
-                    .then(function (res) {
-                        idBook = res.body.book.id;
-                        
-                        // get The Book cerated before
-                        return testServer
-                            .get('/api/book/id/' + idBook)
-                            .expect(200)
-                            .expect('Content-Type', /application\/json/);
-                    })
-                    .then(function (res) {
-                        var headerParams = res.body,
-                            bookParams = headerParams.book;
-                        
-                        // Assertions ti Testing Book
-                        expect(headerParams).to.have.property('book');
-                        _assertBookProperties(bookParams);
-                        expect(bookParams).to.have.property('id').to.equal(idBook);
-                    })
-                    .then(done)
-                    .catch(console.log.bind(console));
+                // get The Book cerated before
+                return testServer
+                    .get('/api/book/id/' + idBook)
+                    .expect(200)
+                    .expect('Content-Type', /application\/json/)
+                .then(function (res) {
+                    var headerParams = res.body,
+                        bookParams = headerParams.book;
+                    
+                    // Assertions ti Testing Book
+                    expect(headerParams).to.have.property('book');
+                    _assertBookProperties(bookParams);
+                    expect(bookParams).to.have.property('id').to.equal(idBook);
+                })
+                .finally(done);
             });
         });
 
         describe('[PUT]', function () {
             it('It shold update stored Book.', function (done) {
-                var idBook = null;
+                var idBook = this.newBook.id;
                 
-                // Create or update Book [POST]
-                testServer
-                    .post('/api/book/save/')
-                    .send({book: _bookTest})
-                    .set('Accept', /application\/json/)
-                    .expect(201)
-                    .then(function (res) {
-                        idBook = res.body.book.id;
-                        
-                        // get The Book cerated before
-                        return testServer
-                            .get('/api/book/id/' + idBook)
-                            .expect(200)
-                            .expect('Content-Type', /application\/json/);
-                    })
-                    .then(function (res) {
-                        var headerParams = res.body,
-                            bookParams = headerParams.book;
-                        
-                        bookParams.title = 'Title book for Nerds';
-                        return testServer
-                            .put('/api/book/id/' + idBook)
-                            .send({book: bookParams})
-                            .expect(200);
-                    })
-                    .then(function (res) {
-                        var headerParams = res.body,
-                            bookParams = headerParams.book;
-                        
-                        // Assertions Testing Book
-                        expect(headerParams).to.have.property('book');
-                        expect(bookParams).to.have.property('title').to.equals('Title book for Nerds');
-                        expect(bookParams).to.have.property('id').to.equal(idBook);
-                    })
-                    .then(done)
-                    .catch(console.log.bind(console));
+                // get The Book cerated before
+                return testServer
+                    .get('/api/book/id/' + idBook)
+                    .expect(200)
+                    .expect('Content-Type', /application\/json/)
+                .then(function (res) {
+                    var headerParams = res.body,
+                        bookParams = headerParams.book;
+                    
+                    bookParams.title = 'Title book for Nerds';
+                    return testServer
+                        .put('/api/book/id/' + idBook)
+                        .send({book: bookParams})
+                        .expect(200);
+                })
+                .then(function (res) {
+                    var headerParams = res.body,
+                        bookParams = headerParams.book;
+                    
+                    // Assertions Testing Book
+                    expect(headerParams).to.have.property('book');
+                    expect(bookParams).to.have.property('title').to.equals('Title book for Nerds');
+                    expect(bookParams).to.have.property('id').to.equal(idBook);
+                })
+                .finally(done);
             });
         });
 
         describe('[DELETE]', function () {
             it('It should delete a book', function (done) {
-                 var idBook = null;
+                 var idBook = this.newBook.id;
                 
-                // Create or update Book [POST]
-                testServer
-                    .post('/api/book/save/')
-                    .send({book: _bookTest})
-                    .set('Accept', /application\/json/)
-                    .expect(201)
-                    .then(function (res) {
-                        idBook = res.body.book.id;
-                        
-                        // get The Book cerated before
-                        return testServer
-                            .get('/api/book/id/' + idBook)
-                            .expect(200)
-                            .expect('Content-Type', /application\/json/);
-                    })
-                    .then(function (res) {
-                        var headerParams = res.body,
-                            bookParams = headerParams.book;
-                        
-                        bookParams.title = 'Title book for Nerds';
-                        // remove a book [DELETE]
-                        return testServer
-                            .delete('/api/book/id/' + idBook)
-                            .expect(200);    
-                    })
-                    .then(function () {
-                        testServer
-                            .get('/api/book/id/' + idBook)
-                            .expect(404);
-                    })
-                    .then(done)
-                    .catch(console.log.bind(console));
+                // get The Book cerated before
+                return testServer
+                    .get('/api/book/id/' + idBook)
+                    .expect(200)
+                    .expect('Content-Type', /application\/json/)
+                .then(function (res) {
+                    var headerParams = res.body,
+                        bookParams = headerParams.book;
+                    
+                    bookParams.title = 'Title book for Nerds';
+                    // remove a book [DELETE]
+                    return testServer
+                        .delete('/api/book/id/' + idBook)
+                        .expect(200);    
+                })
+                .then(function () {
+                    testServer
+                        .get('/api/book/id/' + idBook)
+                        .expect(404);
+                })
+                .finally(done)
+                .catch(console.error.bind(console));
             });
         });
     });
