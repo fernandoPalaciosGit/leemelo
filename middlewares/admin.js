10a825313e02e748b0700488cf6a2e31cac458c3
fernandoPalaciosGit <fernandopalacioslandi@gmail.com>
2015-08-26 19:12:46 +0200

    install CORS middleware into express server

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,5 +1,6 @@
 // list of middlewares that express use
 module.exports = {
+    cors: require('./cors.middleware'),
     staticFiles: require('./static.middleware'),
     bodyParserExtend: require('./bodyParser.extend.middleware'),
     bodyParserJson: require('./bodyParser.json.middleware'),
---
+++ middlewares/cors.middleware.js
@@ -0,0 +1,20 @@
+;(function () {
+   'use strict';
+    
+    var cors = require('cors'),
+        conf = require('./../server/conf'),
+        whitelist = [
+            conf.getserverPath('serverDev'),
+            conf.getserverPath('serverProd'),
+            conf.getserverPath('serverTest')
+        ],
+        corsOptions = {
+            // @var origin {string} path from client request
+            origin: function(origin, callback){
+                var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+                callback(null, originIsWhitelisted);
+            }
+        };
+
+    module.exports = cors(corsOptions);
+}());
