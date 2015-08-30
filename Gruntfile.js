45c9e5635176a90671c4c9160e9d44811e022b10
fernandoPalaciosGit <fernandopalacioslandi@gmail.com>
2015-08-30 22:41:56 +0200

    grunt local deployment proyect

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,14 @@
 (function () {
     'use strict';
     
+    var conf = require('./server/conf');
+
+    conf.server = conf['serverDev'];
     module.exports = function (grunt) {
+        // loading apckages
         grunt.loadNpmTasks('grunt-contrib-jshint');
+        grunt.loadNpmTasks('grunt-shell-spawn');
+        // configuration packages
         grunt.initConfig({
             jshint: {
                 options: {
@@ -17,8 +23,34 @@
                     'test/**/*.js',
                     'website/**/*.js'
                 ]
+            },
+            shell: {
+                options: {
+                    stdout: true,
+                    stderr: true,
+                    failOnError: true,
+                    execOptions: {
+                        maxBuffer: Infinity
+                    }
+                },
+                runDB: {
+                    command: [
+                        'C:', 'mongod'
+                    ].join('&&'),
+                    options: {
+                        async: true
+                    }
+                },
+                runProject: {
+                    command: 'npm start'
+                },
+                openProject: {
+                    command: 'start chrome \"' + conf.getserverPath() + '/landing' + '\"'
+                }
             }
         });
+        // Console instructions
         grunt.registerTask('lint', ['jshint']);
+        grunt.registerTask('run', ['shell:runDB', 'shell:openProject', 'shell:runProject']);
     };
 }());
