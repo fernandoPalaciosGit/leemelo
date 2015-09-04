f73c5586f16bdf5220cf4289a0a2becd456e8830
fernandoPalaciosGit <fernandopalacioslandi@gmail.com>
2015-09-04 22:04:18 +0200

    grunt jshint, jscs rules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,40 @@
 (function () {
     'use strict';
-    
-    var conf = require('./server/conf');
+
+    var conf = require('./server/conf'),
+        lintPaths = [
+            'Gruntfile.js',
+            'app.js',
+            'middlewares/**/*.js',
+            'server/**/*.js',
+            'workers/**/*.js',
+            'test/**/*.js',
+            'website/**/*.js'
+        ];
 
     conf.server = conf['serverDev'];
     module.exports = function (grunt) {
         // loading apckages
         grunt.loadNpmTasks('grunt-contrib-jshint');
         grunt.loadNpmTasks('grunt-shell-spawn');
+        grunt.loadNpmTasks('grunt-jscs');
         // configuration packages
         grunt.initConfig({
+            jscs: {
+                files: {
+                    src: lintPaths
+                },
+                options: {
+                    fix: true,
+                    config: '.jscsrc',
+                    verbose: true
+                }
+            },
             jshint: {
                 options: {
-                    jshintrc: '.jshintrc' 
+                    jshintrc: '.jshintrc'
                 },
-                all: [
-                    'Gruntfile.js',
-                    'app.js',
-                    'middlewares/**/*.js',
-                    'server/**/*.js',
-                    'workers/**/*.js',
-                    'test/**/*.js',
-                    'website/**/*.js'
-                ]
+                all: lintPaths
             },
             shell: {
                 options: {
@@ -50,7 +62,7 @@
             }
         });
         // Console instructions
-        grunt.registerTask('lint', ['jshint']);
+        grunt.registerTask('lint', ['jshint', 'jscs']);
         grunt.registerTask('run', ['shell:runDB', 'shell:openProject', 'shell:runProject']);
     };
 }());
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 ;(function () {
     'use strict';
-    
+
     var _ = require('lodash'),
         cluster = require('cluster'),
         conf = require('./server/conf'),
@@ -20,7 +20,7 @@
             setMasterCluester: function () {
                 var MasterWorker = require('./workers/master.worker'),
                     cpuCount = require('os').cpus().length;
-                     
+
                 this.masterWorker = new MasterWorker(cluster);
                 /**
                  * Create a worker (with cluster) foreach cpu thread
@@ -28,24 +28,24 @@
                 for (var i = 0; i < cpuCount; i++) {
                     this.masterWorker.createSlaveWorker();
                 }
-                
+
                 /**
                  * Once the worker died, reactivate new one, then we got full working cpus
                  */
                 cluster.on('exit', function (worker){
                     this.masterWorker.onWorkerExit(worker);
-                }.bind(this));    
+                }.bind(this));
             },
             /**
              * Manage Slaves Cluster : Execute program in all clusters
-             * For each this Worker we create and run our aplication 
+             * For each this Worker we create and run our aplication
              * We cannot debug into this kind aplications process (there are many http --debug ports)
              */
             initServer: function () {
                 var SlaveWorker = require('./workers/slave.worker'),
                     serverName = APP.env === 'production' ? 'serverProd' : 'serverDev',
                     confServer = _.clone(conf);
-                
+
                 confServer.server = confServer[serverName];
                 this.worker = new SlaveWorker(confServer);
                 this.worker.initalizeConnection();
@@ -57,27 +57,27 @@
             exportTestingServer : function () {
                 var SlaveWorker = require('./workers/slave.worker'),
                     confServer = _.clone(conf);
-                
+
                 confServer.server = confServer['serverTest'];
                 this.worker = new SlaveWorker(confServer);
                 this.worker.initalizeConnection();
-                module.exports = this.worker.app.expressServer;  
+                module.exports = this.worker.app.expressServer;
             }
         };
-    
+
     // retun NODE_ENV default settings
     process.on('exit', APP.finishProcess.bind(APP, {event: 'exit'}));
     process.on('SIGINT', APP.finishProcess.bind(APP, {event: 'SIGINT'}));
     process.on('uncaughtException', APP.finishProcess.bind(APP, {event: 'uncaughtException'}));
-    
+
     // $set "NODE_ENV=testing" && mocha app
     if (APP.env === 'testing') {
         APP.exportTestingServer.call(APP);
-    
+
     // $set "NODE_ENV=production" && nodemon [--debug] app
     } else if (APP.env === 'production' && cluster.isMaster) {
         APP.setMasterCluester.call(APP);
-    
+
     // $set "NODE_ENV=development" && nodemon [--debug] app
     } else {
         APP.initServer.call(APP);
